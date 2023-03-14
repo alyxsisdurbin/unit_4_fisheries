758159c254de7b43a814b4e3b7df163155399f3a
alyxsisdurbin <hdurbin@email.sc.edu>
2023-03-14 11:12:56 -0400

    class notes

--- class notes/class_notes_2023-03-14.R
+++ class notes/class_notes_2023-03-14.R
@@ -24,4 +24,47 @@ data12 = data1 %>%
 data12 = data1 %>% 
   right_join(data2, by="ID")
 
+#inner_join - only brings in data/ ID in which both data frames have this in common
+data12 = data1 %>%
+  inner_join(data2, by="ID")
+
+# full_join - retains all data regardless of common data
+data12 = data1 %>%
+  full_join(data2, by="ID")
+
+##### always check the dimensions of new data
+
+# semi_join - not adding any new information
+data12_semi = data1 %>%
+  semi_join(data2, by="ID")
+data12_semi
+
+# anti_join - when you want to know what is missing
+data12_anti = data1 %>%
+  anti_join(data2, by="ID")
+data12_anti
+
+##### wider vs. longer
+survey = data.frame(quadrat_id = c(101, 102, 103, 104),
+                    barnacle_n = c(2, 11, 8, 27),
+                    chiton_n = c(1, 0, 0, 2),
+                    mussel_n = c(0, 1, 1, 4))
+long = survey %>% 
+  pivot_longer(cols=c("barnacle_n", "chiton_n", "mussel_n"), 
+               names_to="taxon", 
+               values_to="counts")
+
+wide = long %>%
+  pivot_wider(names_from=taxon, 
+              values_from=counts)
+
+# exercise 1.2
+ggplot(data=survey) +
+  geom_point(x=quadrat_id, y=barnacle_n) +
+  geom_point(x=quadrat_id, y=chiton_n) +
+  geom_point(x=quadrat_id, y=mussel_n)
+
+
+ggplot(data=long) +
+  geom_point(aes(x=quadrat_id, y=counts, color=taxon))
 
